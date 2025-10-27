cb56a38b29d280e6c9fc9a45e91e5fe6689c467e
XC <schosnablue@gmail.com>
2025-10-27 16:47:56 +0800

    Add back navigation arrow to Pokemon detail screen

--- app/src/main/java/com/joeho/pokedexapp/ui/detail/PokemonDetailFragment.kt
+++ app/src/main/java/com/joeho/pokedexapp/ui/detail/PokemonDetailFragment.kt
@@ -2,12 +2,14 @@ package com.joeho.pokedexapp.ui.detail
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import coil.load
 import com.joeho.pokedex.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,11 +23,16 @@ class PokemonDetailFragment : Fragment(R.layout.fragment_pokemon_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val nameArg = arguments?.getString("name") ?: return
+        val backButton = view.findViewById<ImageButton>(R.id.buttonBack)
         val image = view.findViewById<ImageView>(R.id.imageView)
         val name = view.findViewById<TextView>(R.id.textName)
         val types = view.findViewById<TextView>(R.id.textTypes)
         val abilities = view.findViewById<TextView>(R.id.textAbilities)
         val favoriteIcon = view.findViewById<ImageView>(R.id.favoriteIcon)
+
+        backButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
 
         favoriteIcon.setOnClickListener {
             viewModel.toggleFavorite(nameArg)
